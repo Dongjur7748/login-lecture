566264771f7d8c105b05d805b84374b0d281d7c6
Dongjur7748 <june7748@gmail.com>
2023-07-29 17:17:55 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,15 @@
 "use stirct";
 
-console.log("hello");
-console.log("bye");
+
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+}
