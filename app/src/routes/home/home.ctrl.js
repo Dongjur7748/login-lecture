cf6a1b6ebad6538a2d2a1a77f0c8340efff0a43b
Dongjur7748 <june7748@gmail.com>
2023-07-30 17:23:03 +0900

    백엔드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,43 @@
 "use strict";
 
-const home = (req, res) => {
-    res.render("home/index");
+const output = {
+    home: (req, res) => {
+        res.render("home/index");
+    },
+    
+    login: (req, res) => {
+        res.render("home/login");
+    },
 };
 
-const login = (req, res) => {
-    res.render("home/login");
+const users = {
+    id: ["june", "나개발", "김팀장"],
+    psword: ["1234", "12345", "123456"],
+};
+
+const process = {
+    login: (req,res) => {
+        const id = req.body.id,
+            psword = req.body.psword;
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword) {
+                return res.json({
+                    succes: true,
+                });
+            }
+        }
+
+        return res.json ({
+            succes: false,
+            msg: "로그인을 실패했습니다.",
+        });
+    },
+    
 };
 
 module.exports = {
-    home,
-    login,
+    output,
+    process,
 };
